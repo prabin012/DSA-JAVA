f64964f2728838dbf132271e0dc8dfb67e8301e2
Prabin Kumar Mahato <110293578+prabin012@users.noreply.github.com>
2023-09-07 08:58:14 +0000

    updating

--- Array/CreateArrays.java
+++ Array/CreateArrays.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class CreateArrays {
     public static void main(String[] args) {
-        System.out.println("Hello owrld");
+
+        // Creating array 
+        int number[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+
+
+        //Take item of array from user
+        for(int i=0; i<number.length;i++){
+            number[i] = sc.nextInt();
+             
+        }
+
+        //Print itme of array
+        for(int i=0; i< number.length; i++){
+            System.out.print(number[i] + " ");
+        }
+
+        //PRint nextLine
+        System.out.println();
+       
     }
 }
